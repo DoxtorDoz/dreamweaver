9c76cd729ab5b6c1297bb63d6f003e3a1fc52ea2
Antonio Gonzalez Capel <gonza359@gmail.com>
2025-06-15 17:55:52 +0200

    first commit

---
+++ src/test/java/com/dreamweaver/dreamweaver/DreamweaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamweaver.dreamweaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamweaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
